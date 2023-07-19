74672555df546034c8bb29fbfcd66b637b3b1d93
mwntech-edu <mwntechnology@gmail.com>
2023-07-19 16:36:58 +0900

    Tempalte 2

--- Template/Main1.cpp
+++ Template/Main1.cpp
@@ -6,10 +6,26 @@ T Add(T num1, T num2) {
 	return num1 + num2;
 }
 
+template <class T1, class T2>
+void ShowData(double num) {
+	cout << (T1)num << ", " << (T2)num << endl;
+}
+
 int main() {
 	cout << Add<int>(15, 20) << endl;
 	cout << Add<double>(2.9, 3.7) << endl;
 	cout << Add<int>(3.2, 3.2) << endl;
 	cout << Add<double>(3.14, 2.75) << endl;
+
+	cout << Add(15, 20) << endl;
+	cout << Add(2.9, 3.7) << endl;
+	cout << Add(3.2, 3.2) << endl;
+	cout << Add(3.14, 2.75) << endl;
+
+	ShowData<char, int>(65);
+	ShowData<char, int>(67);
+	ShowData<char, double>(68.9);
+	ShowData<short, double>(69.2);
+	ShowData<short, double>(70.4);
 	return 0;
 }
