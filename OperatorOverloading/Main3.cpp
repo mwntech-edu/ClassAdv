01947ad8ac6c4bf84b31113db82ad9fe1f0df203
mwntech-edu <mwntechnology@gmail.com>
2023-07-19 13:01:52 +0900

    boundary check Point array

--- OperatorOverloading/Main3.cpp
+++ OperatorOverloading/Main3.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
+class Point {};
+
+//#define POINT_PTR Point*
+//Point** arr;
+//POINT_PTR* arr;
+
 class BoundCheckIntArray {
 private:
 	int* arr;
@@ -11,7 +17,7 @@ public:
 	int& operator[](int idx) {
 		if (idx < 0 || idx >= arrlen) {
 			cout << "Array index out of bound exception" << endl;
-			exit(1);
+			//exit(1);
 		}
 		return arr[idx];
 	}
@@ -19,17 +25,15 @@ public:
 };
 int main() {
 	int array[5] = { 1, 2, 3, 4, 5 };
-	int a = 10, b = 20;
-	int c = a + b;
 	BoundCheckIntArray arr(5);
 	for (int i = 0; i < 5; i++)
 	{
 		arr[i] = (i + 1) * 11;
 	}
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < 8; i++)
 	{
 		cout << array[i] << endl;
-		cout << arr[i] << endl;
+		cout << arr[i] << " " << arr.operator[](i) << endl;
 	}
 	return 0;
 }
