8622b7b7a1ddfa9bfd9842bd4e23f189919628ab
mwntech-edu <mwntechnology@gmail.com>
2023-07-19 17:01:57 +0900

    Tempalte specialization

--- Template/Main1.cpp
+++ Template/Main1.cpp
@@ -11,6 +11,21 @@ void ShowData(double num) {
 	cout << (T1)num << ", " << (T2)num << endl;
 }
 
+template <typename T>
+T Max(T a, T b) { return a > b ? a : b; }
+
+template<>
+char* Max(char* a, char* b) {
+	cout << "char* Max<char*>(char* a, char* b)" << endl;
+	return strlen(a) > strlen(b) ? a : b;
+}
+
+template<>
+const char* Max(const char* a, const char* b) {
+	cout << "const char* Max<char*>(char* a, char* b)" << endl;
+	return strcmp(a, b) > 0 ? a : b;
+}
+
 int main() {
 	cout << Add<int>(15, 20) << endl;
 	cout << Add<double>(2.9, 3.7) << endl;
@@ -27,5 +42,13 @@ int main() {
 	ShowData<char, double>(68.9);
 	ShowData<short, double>(69.2);
 	ShowData<short, double>(70.4);
+
+	cout << Max(11, 15) << endl;
+	cout << Max('T', 'Q') << endl;
+	cout << Max(3.5, 7.5) << endl;
+	cout << Max("Simple", "Best") << endl;
+	char str1[] = "Simple";
+	char str2[] = "Best";
+	cout << Max(str1, str2) << endl;
 	return 0;
 }
