2e17b659c72f528d78bf1ae38587416fa1079978
mwntech-edu <mwntechnology@gmail.com>
2023-07-20 13:06:03 +0900

    template default value

--- Template/Main3.cpp
+++ Template/Main3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-template <typename T, int len>
+template <typename T=int, int len=7>
 class SimpleArray {
 private:
 	T arr[len];
@@ -14,8 +14,13 @@ public:
 int main() {
 	SimpleArray<int, 5> i5arr1;
 	SimpleArray<int, 7> i7arr1;
-
 	//i5arr1 = i7arr1; // (X)
-
+	SimpleArray<> arr;
+	for (int i = 0; i < 7; i++)
+	{
+		arr[i] = i + 1;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
 	return 0;
 }
