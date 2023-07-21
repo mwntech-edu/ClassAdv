63a62c40c0ad3f3f544539451f53d50d93e694e7
mwntech-edu <mwntechnology@gmail.com>
2023-07-21 10:25:34 +0900

    exception stack

--- Exception/Main2.cpp
+++ Exception/Main2.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void Divide(int num1, int num2) {
+	if (num2 == 0)
+		throw num2;
+
+	cout << "³ª´°¼ÀÀÇ ¸ò: " << num1 / num2 << endl;
+	cout << "³ª´°¼ÀÀÇ ³ª¸ÓÁö: " << num1 % num2 << endl;
+}
+
 int main() {
 	int num1, num2;
 	cout << "µÎ °³ÀÇ ¼ýÀÚ ÀÔ·Â: ";
 	cin >> num1 >> num2;
 
 	try {
-		if (num2 == 0) 
-			throw num2;
-		cout << "³ª´°¼ÀÀÇ ¸ò: " << num1 / num2 << endl;
-		cout << "³ª´°¼ÀÀÇ ³ª¸ÓÁö: " << num1 % num2 << endl;
+		Divide(num1, num2);
+		cout << "³ª´°¼ÀÀ» ¸¶ÃÆ½À´Ï´Ù." << endl;
 	}
 	catch (int expn) {
 		cout << "Á¦¼ö´Â " << expn << "ÀÌ µÉ ¼ö ¾ø½À´Ï´Ù." << endl;
