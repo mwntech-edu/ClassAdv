fe75a05bfa683cd7d0012b11db48dc41a0fb6786
mwntech-edu <mwntechnology@gmail.com>
2023-07-21 09:38:27 +0900

    exception

--- Template/Main3.cpp
+++ Template/Main3.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 template <typename T=int, int len=7>
 class SimpleArray {
 private:
@@ -10,13 +9,22 @@ public:
 		return arr[idx];
 	}
 };
-
 template <typename T>
 void ShowStaticValue() {
 	static T num = 0;
 	num += 1;
 	cout << num << " ";
 }
+template <typename T>
+class SimpleStaticMem {
+private:
+	static T mem;
+public:
+	void AddMem(int num) { mem += num; }
+	void ShowMem() { cout << mem << endl; }
+};
+template <typename T>
+T SimpleStaticMem<T>::mem = 0;
 
 int main() {
 	SimpleArray<int, 5> i5arr1;
@@ -29,19 +37,28 @@ int main() {
 		cout << arr[i] << " ";
 	}
 	cout << endl;
+	ShowStaticValue<int>();
+	ShowStaticValue<int>();
+	ShowStaticValue<int>();
+	cout << endl;
+	ShowStaticValue<long>();
+	ShowStaticValue<long>();
+	ShowStaticValue<long>();
+	cout << endl;
+	ShowStaticValue<double>();
+	ShowStaticValue<double>();
+	ShowStaticValue<double>();
+	cout << endl;
+	ShowStaticValue<int>();
 
-	ShowStaticValue<int>();
-	ShowStaticValue<int>();
-	ShowStaticValue<int>();
-	cout << endl;
-	ShowStaticValue<long>();
-	ShowStaticValue<long>();
-	ShowStaticValue<long>();
-	cout << endl;
-	ShowStaticValue<double>();
-	ShowStaticValue<double>();
-	ShowStaticValue<double>();
-	cout << endl;
-	ShowStaticValue<int>();
+	SimpleStaticMem<int> intSSM;
+	intSSM.AddMem(2);
+	intSSM.ShowMem();
+	intSSM.AddMem(2);
+	intSSM.ShowMem();
+	SimpleStaticMem<float> floatSSM;
+	floatSSM.AddMem(3.14);
+	floatSSM.AddMem(0.1);
+	floatSSM.ShowMem();
 	return 0;
 }
