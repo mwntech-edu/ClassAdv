82c1115234d90e7f080e3de63d2c3889e6422947
mwntech-edu <mwntechnology@gmail.com>
2023-07-21 10:00:54 +0900

    exception try catch

--- Exception/Main1.cpp
+++ Exception/Main1.cpp
@@ -6,14 +6,16 @@ int main() {
 	cout << "두 개의 숫자 입력: ";
 	cin >> num1 >> num2;
 
-	if (num2 == 0) {
-		cout << "제수는 0이 될 수 없습니다." << endl;
-		cout << "프로그램을 다시 실행하세요." << endl;
-	}
-	else {
+	try {
+		if (num2 == 0) 
+			throw num2;
+			
 		cout << "나눗셈의 몫: " << num1 / num2 << endl;
 		cout << "나눗셈의 나머지: " << num1 % num2 << endl;
 	}
-	
+	catch (int expn) {
+		cout << "제수는 " << expn << "이 될 수 없습니다." << endl;
+		cout << "프로그램을 다시 실행하세요." << endl;
+	}
 	return 0;
 }
